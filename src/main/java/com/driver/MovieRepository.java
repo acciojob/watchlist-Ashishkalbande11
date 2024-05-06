3c7972bcb6c53b3b97031808d3c6cfb90b40e246
Ashishkalbande11 <ashishkalbande60>
2024-05-06 07:51:55 +0530

    1

--- src/main/java/com/driver/MovieRepository.java
+++ src/main/java/com/driver/MovieRepository.java
@@ -19,28 +19,38 @@ public class MovieRepository {
 
     public void saveMovie(Movie movie){
         // your code here
+        movieMap.put(movie.getName(), movie);
+
     }
 
     public void saveDirector(Director director){
         // your code here
+        directorMap.put(director.getName(), director);
     }
 
     public void saveMovieDirectorPair(String movie, String director){
-        if(movieMap.containsKey(movie) && directorMap.containsKey(director)){
-            // your code here
+        List<String> movieByDirector = directorMovieMapping.get(director);
+        if(movieByDirector == null){
+            movieByDirector = new ArrayList<>();
+            directorMovieMapping.put(director, movieByDirector);
         }
+        movieByDirector.add(movie);
     }
 
     public Movie findMovie(String movie){
         // your code here
+        return movieMap.get(movie);
     }
 
     public Director findDirector(String director){
         // your code here
+        return directorMap.get(director);
     }
 
     public List<String> findMoviesFromDirector(String director){
         // your code here
+        return directorMovieMapping.getOrDefault(director, new ArrayList<>());
+
     }
 
     public List<String> findAllMovies(){
@@ -49,9 +59,14 @@ public class MovieRepository {
 
     public void deleteDirector(String director){
         // your code here
+        directorMap.remove(director);
+        directorMovieMapping.remove(director);
+
     }
 
     public void deleteAllDirector(){
         // your code here
+        directorMovieMapping.clear();
+        movieMap.clear();
     }
 }
